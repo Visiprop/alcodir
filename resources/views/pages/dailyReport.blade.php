6072cb5bdfb4ed232afbac925471a99ec120cf10
irfan <m.irfan0081@gmail.com>
2020-05-31 21:02:26 +0700

    update daily report

--- resources/views/pages/dailyReport.blade.php
+++ resources/views/pages/dailyReport.blade.php
@@ -27,8 +27,9 @@
                 <h4 class="m-b-0 text-white">Daily Report Form </h4>
             </div>
             <div class="card-body">
-                <form action="{{ route('dailyreport.submit') }}" method="POST" class="form-horizontal">
+                <form action="{{ route('dailyreport.submit') }}" method="POST" class="form-horizontal" onsubmit="submitForm()">
                     @csrf
+                    <input type="text" name="indexes" id="taskIndexes" hidden>
                     <div class="form-body">
                         <h3 class="card-title">Report</h3>
                         <hr>
@@ -43,17 +44,43 @@
                             <div class="col-md-6">
                                 <div class="form-group has-label">
                                     <label class="control-label">Advice</label>
-                                    <textarea name="advice"class="form-control" rows="5"></textarea>
+                                    <textarea name="advice" class="form-control" rows="5"></textarea>
                                     <small class="form-control-feedback"> Advice for All of Us (Optional)</small> </div>
                             </div>
                             <!--/span-->
                         </div>
-                        <!--/row-->                                        
-                    </div>
-                    <div class="form-actions">
-                                                            
-                        <button type="submit" class="btn btn-success"> <i class="fa fa-check"></i> Submit</button>
+                        <!--/row-->
+                        <hr>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <label>Task</label>
+                            </div>
+                        </div>
+                        <div class="row" id="inputTask-Warp">
+                            <div class="col-md-12 col-lg-12" >
+                                <div class="row" id="warpTask_0">
+                                    <div class="col-lg-12 col-md-12" >
+                                        <div class="input-group bootstrap-touchspin">
+                                            <input id="indexTask_0" name="indexTask_0"  type="text" placeholder="Example: Create a design for PT.example." data-bts-button-down-class="btn btn-secondary btn-outline" data-bts-button-up-class="btn btn-secondary btn-outline" class="form-control" style="display: block;">
+                                            
+                                            <div class="input-group-append">
+                                                <button hidden id="spliceTask_0" class="btn btn-danger bootstrap-touchspin-up" type="button" onclick="spliceTask(0)">-</button>
+                                                <button Task="background-color: #0f4ba4;" id="plusTask_0" class="btn btn-primary bootstrap-touchspin-up" type="button" onclick="addMoreTask()">+</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>   
+                    </div><hr>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-actions">
+                                                                    
+                                <button type="submit" class="btn btn-success"> <i class="fa fa-check"></i> Submit</button>
 
+                            </div>
+                        </div>
                     </div>
                 </form>
             </div>
@@ -126,6 +153,71 @@
 @endsection
 
 @section('script')
-    <script>$('#dailyReportTable').DataTable();</script>
+    <script>
+        $('#dailyReportTable').DataTable();
+        // VARIABLE DESIGN Task
+        var xTask = 1 //INDEXING INPUT DESIGN Task
+        var counterWarp_Task = [0] //INDEX STORAGE DESIGN Task
+        var warpInput_Task = $("#inputTask-Warp") //WARP INPUT DESIGN Task
+
+        // START FUNC DESIGN Task
+        function addMoreTask() {
+            warpInput_Task.append(`
+            <div class="col-lg-12 col-md-12" id="warpTask_${xTask}" >
+                <br>
+                <div class="input-group bootstrap-touchspin">
+                    <input id="indexTask_${xTask}" name="indexTask_${xTask}"  type="text" placeholder="Example: Create a design for PT.example." data-bts-button-down-class="btn btn-secondary btn-outline" data-bts-button-up-class="btn btn-secondary btn-outline" class="form-control" Task="display: block;">
+                    
+                    <div class="input-group-append">
+                        <button hidden id="spliceTask_${xTask}" class="btn btn-danger bootstrap-touchspin-up" type="button" onclick="spliceTask(${xTask})">-</button>
+                        <button id="plusTask_${xTask}" class="btn btn-primary bootstrap-touchspin-up" type="button" onclick="addMoreTask()">+</button>
+                    </div>
+                </div>
+            </div>`)
+            if(counterWarp_Task.length == 1) {
+                $(`#spliceTask_${counterWarp_Task[0]}`).removeAttr('hidden')
+                $(`#plusTask_${counterWarp_Task[0]}`).attr('hidden', true)
+            } else {
+                $(`#spliceTask_${counterWarp_Task[ counterWarp_Task.findIndex(_Task => _Task == (xTask - 1)) ]}`).removeAttr('hidden')
+                $(`#plusTask_${counterWarp_Task[ counterWarp_Task.findIndex(_Task => _Task == (xTask - 1)) ]}`).attr('hidden', true)
+            }
+            counterWarp_Task.push(xTask)
+            let temps = ''
+            counterWarp_Task.map((_val, ind) => {
+                if(counterWarp_Task.length == (ind+1)) {
+                    temps += ''+_val
+                } else {
+                    temps += ''+_val+'/'
+                }
+            })
+            $("#taskIndexes").val(temps)
+            xTask++;
+        }
+
+        async function spliceTask( warpNumber ) {
+            if(counterWarp_Task.length > 1) {
+                $(`#warpTask_${warpNumber}`).remove()
+                await counterWarp_Task.splice(counterWarp_Task.findIndex( _warpNumber => _warpNumber == warpNumber ), 1)
+                if(counterWarp_Task.length == 1) {
+                    $(`#spliceTask_${counterWarp_Task[0]}`).attr("hidden", true);
+                }
+                let temps = ''
+                counterWarp_Task.map((_val, ind) => {
+                    if(counterWarp_Task.length == (ind+1)) {
+                        temps += ''+_val
+                    } else {
+                        temps += ''+_val+'/'
+                    }
+                })
+                $("#taskIndexes").val(temps)
+            }
+        }
+
+        function submitForm() {
+            console.log('return Submited')
+            return false
+        }
+        // END FUNC DESIGN Task
+    </script>
     
 @endsection
